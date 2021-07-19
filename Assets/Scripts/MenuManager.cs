e2192d466e7bc35ae76011d6c0f413f38375e9a0
baneofsmite <baneofsmite@gmail.com>
2021-07-20 00:31:10 +0800

    Started on Main Menu UI, Finished design and play button

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
-    // Start is called before the first frame update
+
     public void inGameButton(int id) {
         switch (id) {
-            //1-3 GameOver Buttons
+            //1-3 GameOver Buttons\\
             case 1: //Replay
                 AchievementManager.instance.SaveData();
                 SceneManager.LoadScene(1);
                 break;
             case 2: //Achievements
-                
+
                 break;
             case 3: //Home
+                AchievementManager.instance.SaveData();
                 SceneManager.LoadScene(0);
                 break;
             case 4:
+                break;
+
+            //5-7 Main Menu's Button Controller\\
+            case 5: //Play
+                SceneManager.LoadSceneAsync(1);
+                break;
+            case 6: //Options
+                break;
+            case 7: //Achievements
                 break;
         }
     }
